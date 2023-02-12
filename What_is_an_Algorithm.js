89ec211d24380b85c190e674c64cd40f5d6d4344
kazi naeem rayhan <kazinaeemrayhan@gmail.com>
2023-02-12 22:05:27 +0600

    first commit

---
+++ What_is_an_Algorithm.js
@@ -0,0 +1,13 @@
+// What is an Algorithm?
+// Ans: An algorithm is a set of well-defined instructions to solve a particular problem.
+// একটি অ্যালগরিদম একটি নির্দিষ্ট সমস্যা সমাধানের জন্য সুনির্দিষ্ট নির্দেশাবলীর একটি সেট।
+
+// Characteristics of the algorithm?
+// Ans: Well-defined input and outputs. Each step should be clear and unambiguous. Language independent.
+
+
+// why Algorithm?
+// Ans: As a developer, we are going to come across problems that we need to solve.
+// Learning an algorithms translates to learning different techniques to efficiently solve those problems.
+// A problem can be solved in many ways using different algorithms.
+// Each algorithm comes with its own tradeoffs when it comes to performance.
